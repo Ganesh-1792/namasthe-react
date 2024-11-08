29bdd86581a949c2187111233df0253bbffbe73d
Ganesh Kumar Asaithambi <ganeshkumar.asaithambi@impigertech.com>
2024-11-08 12:27:56 +0530

    create headers and static resto card

--- App.js
+++ App.js
@@ -1,9 +1,65 @@
-const heading = React.createElement('h1',
-    {id: 'heading'},
-    'Hello World in react js'
-);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-console.log(heading);
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className = 'logo-container'>
+                <img className='logo' src='https://img.freepik.com/free-vector/delivery-service-illustrated_23-2148505081.jpg?t=st=1730894722~exp=1730898322~hmac=6920582a828b8f2685387a87f75647965f90b48bb5ed078a36d6fd546b22513c&w=740' />
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestoCard = () => {
+    return (
+        <div className='restocard'>
+            <img className='resto-logo' src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2024/4/9/dabf99d0-5c51-480b-9895-bce02d2bde9f_e9ffa04d-e57a-4548-a085-0cc9c2e465cc.JPG" alt="rest-logo" />
+            <h2>Megana Foods</h2>
+            <h4>Indian, Chinese, Arabian</h4>
+            <h4>4.4 Starts</h4>
+            <h4>28 minutes</h4>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='card-container'>
+            <RestoCard/>
+            <RestoCard/>
+            <RestoCard/>
+            <RestoCard/>
+            <RestoCard/>
+            <RestoCard/>
+            <RestoCard/>
+            <RestoCard/>
+            <RestoCard/>
+            <RestoCard/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header />
+            <Body />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<AppLayout/>);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 #Namasthe React
+
+#parcel
+- Dev build
+- Local server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Catching - Faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Support older browsers
+- Diagnostics
+- Error Handling
+- Https
+- Tree Shaking - Removed unused codes
+- Dev build and production build 
